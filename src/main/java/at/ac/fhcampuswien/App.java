baf4b23cf28d2962ebf9fe1a1d1d839329298b64
Rene <rene.guenes@stud.fh-campuswien.ac.at>
2022-05-03 11:19:30 +0200

    filled some empty Method bodys in Menu.java

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -2,5 +2,8 @@ package at.ac.fhcampuswien;
 
 public class App {
     public static void main(String[] args) {
+        AppController controller = new AppController();
+        Menu menu = new Menu();
+        menu.start();
     }
 }
--- src/main/java/at/ac/fhcampuswien/Menu.java
+++ src/main/java/at/ac/fhcampuswien/Menu.java
@@ -1,32 +1,51 @@
 package at.ac.fhcampuswien;
 
+import java.util.Scanner;
+
 public class Menu {
-    private AppController controller;
+    private AppController controller = new AppController();
     private static final String INVALID_INPUT_MESSAGE = "";
-    private static final String EXIT_MESSAGE = "";
+    private static final String EXIT_MESSAGE = "Bye bye!";
 
     public void start() {
-
+        printMenu();
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        handleInput(input);
     }
 
     private void handleInput(String input) {
-
+        switch (input) {
+            case "a" -> {
+                getTopHeadlinesAustria(controller);
+            }
+            case "b" -> {
+                getAllNewsBitcoin(controller);
+            }
+            case "y" -> {
+                getArticleCount(controller);
+            }
+            case "q" -> {
+                printExitMessage();
+            }
+        }
     }
 
     private void getArticleCount(AppController ctrl) {
-
+        controller.getArticleCount();
+        System.out.println("1");
     }
 
     private void getTopHeadlinesAustria(AppController ctrl) {
-
+        System.out.println("Austria");
     }
 
     private void getAllNewsBitcoin(AppController ctrl) {
-
+        System.out.println("bitcoin");
     }
 
     private static void printExitMessage() {
-
+        System.out.println(EXIT_MESSAGE);
     }
 
     private static void printInvalidInputMessage() {
@@ -41,7 +60,7 @@ public class Menu {
 
 
                 Enter what you want to do:
-                
+                                
                 a: Get top headlines from Austria
                 b: Get all news about bitcoin
                 y: Count all articles
